0cf4e26694457c67d010d85885cb938b82ed59fd
Aliyu Kamilu <aliyukamilu002@gmail.com>
2022-09-23 23:03:57 +0100

    pushed to deploy client

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,11 +17,11 @@ const Navbar = () => {
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+        {["Exchange", "Wallets"].map((item, index) => (
           <NavBarItem key={item + index} title={item} />
         ))}
         <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
+          Get Started
         </li>
       </ul>
       <div className="flex relative">
